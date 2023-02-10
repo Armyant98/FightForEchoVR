628b66e63e91caf390fec5066ace34a9ac515559
Armyant98 <102646883+Armyant98@users.noreply.github.com>
2023-02-10 02:59:38 -0500

    Updated title symbol, Page alignment fix
    Added empty div that extends viewport width in order to prevent shifting of wrapper content to the left. Adding the empty div allows for centering and extending the wrapper across allowing for double background parallax effect to occur

---
+++ assets/js/AltScrollAnimations.js
@@ -0,0 +1,25 @@
+
+let scrollpos = window.scrollY;
+const navigation = document.getElementById("nav");
+
+
+function add_class_on_scroll() {
+    navigation.classList.add("colorOnScroll");
+}
+
+function remove_class_on_scroll() {
+    navigation.classList.remove("colorOnScroll");
+}
+
+window.addEventListener('scroll', function(){ 
+    //Here you forgot to update the value
+    scrollpos = window.scrollY;
+
+    if(scrollpos > 680){
+        add_class_on_scroll();
+    }
+    else {
+        remove_class_on_scroll();
+    }
+    console.log(scrollpos);
+});
